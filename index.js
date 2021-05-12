bc816cda6210e3fda8d399c75297393f2aa3c6cc
Jonas Luebbers <jonaskluebbers@gmail.com>
2021-05-12 12:35:45 -0400

    Remove env

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 app.use(cors());
--- init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE mouse (
-  ID SERIAL PRIMARY KEY,
-  clicks INT NOT NULL,
-  scroll INT NOT NULL
-);
-
-INSERT INTO mouse (clicks, scroll) VALUES  (0, 0);
